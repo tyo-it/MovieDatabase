79afb7affaa3c3d00cce7cd9f408a5b4e802d13f
tyo-it <istantyo.0@gmail.com>
2020-11-06 11:26:33 +0700

    feature: create MovieRepository

---
+++ app/src/main/java/com/ittyo/model/Movie.kt
@@ -0,0 +1,39 @@
+package com.ittyo.model
+
+import com.ittyo.repository.remote.MovieResponse
+
+data class Movie (
+    val id : Int,
+    val popularity : Double,
+    val voteCount : Int,
+    val video : Boolean,
+    val posterPath : String,
+    val adult : Boolean,
+    val backdropPath : String,
+    val originalLanguage : String,
+    val originalTitle : String,
+    val genreIds : List<Int>,
+    val title : String,
+    val voteAverage : Double,
+    val overview : String,
+    val releaseDate : String
+)
+
+fun fromResponse(movieResponse: MovieResponse): Movie {
+    return Movie(
+        id = movieResponse.id,
+        popularity = movieResponse.popularity,
+        voteCount = movieResponse.voteCount,
+        video = movieResponse.video,
+        posterPath = movieResponse.posterPath,
+        adult = movieResponse.adult,
+        backdropPath = movieResponse.backdropPath,
+        originalLanguage = movieResponse.originalLanguage,
+        originalTitle = movieResponse.originalTitle,
+        genreIds = movieResponse.genreIds,
+        title = movieResponse.title,
+        voteAverage = movieResponse.voteAverage,
+        overview = movieResponse.overview,
+        releaseDate = movieResponse.releaseDate
+    )
+}
---
+++ app/src/main/java/com/ittyo/repository/remote/constant.kt
@@ -0,0 +1,4 @@
+package com.ittyo.repository.remote
+
+const val SEARCH_PAGE_SIZE = 20
+const val SEARCH_STARTING_PAGE_INDEX = 1
