0155b26e97da63a43b4dcc2079b9b6df577cd543
tyo-it <istantyo.0@gmail.com>
2020-11-06 10:26:13 +0700

    feature: create MovieDatabaseService

---
+++ app/src/main/java/com/ittyo/repository/remote/MovieDatabaseService.kt
@@ -0,0 +1,12 @@
+package com.ittyo.repository.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieDatabaseService {
+    @GET("search/movie")
+    suspend fun searchMovie(
+        @Query("query") query: String,
+        @Query("page") page: Int
+    ): PageResponse
+}
---
+++ app/src/main/java/com/ittyo/repository/remote/MovieDatabaseServiceFactory.kt
@@ -0,0 +1,58 @@
+package com.ittyo.repository.remote
+
+import com.google.gson.Gson
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object MovieDatabaseServiceFactory {
+
+    fun build(apiKey: String, isDebug: Boolean): MovieDatabaseService {
+        val httpLoggingInterceptor = makeLoggingInterceptor(isDebug)
+        val tokenInterceptor = TokenInterceptor(apiKey)
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(tokenInterceptor)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+
+        return makeMovieDatabaseService(
+            okHttpClient,
+            Gson()
+        )
+    }
+
+    private fun makeMovieDatabaseService(okHttpClient: OkHttpClient, gson: Gson): MovieDatabaseService {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+        return retrofit.create(MovieDatabaseService::class.java)
+    }
+
+    private fun makeLoggingInterceptor(isDebug: Boolean): HttpLoggingInterceptor {
+        val logging = HttpLoggingInterceptor()
+        logging.level = if (isDebug) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+        return logging
+    }
+}
+
+class TokenInterceptor(private val apiKey: String) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var original = chain.request()
+        val url = original.url.newBuilder().addQueryParameter("api_key", apiKey).build()
+        original = original.newBuilder().url(url).build()
+        return chain.proceed(original)
+    }
+}
