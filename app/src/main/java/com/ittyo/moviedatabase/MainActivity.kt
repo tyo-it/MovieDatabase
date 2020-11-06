c4ea70f723019595275cd92c9fbd526babdd3406
tyo-it <istantyo.0@gmail.com>
2020-11-06 18:38:40 +0700

    feature: create movieList in MainActivity

--- app/src/main/java/com/ittyo/moviedatabase/MainActivity.kt
+++ app/src/main/java/com/ittyo/moviedatabase/MainActivity.kt
@@ -2,19 +2,33 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
  class MainActivity : AppCompatActivity() {
 
+    private lateinit var movieListAdapter: MovieListAdapter
+
     private val searchMovieViewModel by viewModels<SearchMovieViewModel>()
+    private var searchJob: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        movieListAdapter = MovieListAdapter()
+        movie_list.adapter = movieListAdapter
+        movie_list.layoutManager = GridLayoutManager(this, 2)
+
         searchMovieViewModel.lastSearchResult.observe(this) { pagingData ->
-            Log.d("PAGING", pagingData.toString())
+            searchJob?.cancel()
+            searchJob = lifecycleScope.launch {
+                movieListAdapter.submitData(pagingData)
+            }
         }
 
         searchMovieViewModel.searchMovie("superman")
---
+++ app/src/main/java/com/ittyo/moviedatabase/MovieListAdapter.kt
@@ -0,0 +1,50 @@
+package com.ittyo.moviedatabase
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ittyo.moviedatabase.model.Movie
+
+class MovieListAdapter: PagingDataAdapter<Movie, MovieListAdapter.ViewHolder>(MOVIE_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.view_item_movie, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val movie = getItem(position)
+
+        holder.movieName.text = movie?.originalTitle
+        holder.movieReleaseDate.text = movie?.releaseDate
+        holder.movieOverview.text = movie?.overview
+
+        Glide.with(holder.itemView.context)
+            .load("http://image.tmdb.org/t/p/w185/${movie?.posterPath}")
+            .into(holder.moviePoster)
+    }
+
+    companion object {
+        private val MOVIE_COMPARATOR = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean =
+                oldItem == newItem
+        }
+    }
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val moviePoster: ImageView = view.findViewById(R.id.movie_poster)
+        val movieName: TextView = view.findViewById(R.id.movie_name)
+        val movieReleaseDate: TextView = view.findViewById(R.id.movie_release_date)
+        val movieOverview: TextView = view.findViewById(R.id.movie_overview)
+    }
+}
